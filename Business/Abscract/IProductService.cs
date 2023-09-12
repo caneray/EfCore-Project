7a279744a3599cd3b39d01313452c602524924a8
caner ay <caneray34@hotmail.com>
2023-09-12 13:38:44 +0300

    Add project files.

---
+++ Business/Abscract/IProductService.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abscract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id); //kategori idsine göre ürünü getir.
+        List<Product> GetByUnitPrice(decimal min, decimal max); //şu fiyat aralığındaki ürünleri getir.
+    }
+}
